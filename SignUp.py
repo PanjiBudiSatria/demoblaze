de8f5aee0b6bd249807bf842e98fade7a2d9fa1d
Panji Budi Satria <panjibudi467@gmail.com>
2023-11-16 13:40:19 +0700

    Sign Up Test

---
+++ SignUp.py
@@ -0,0 +1,45 @@
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+from selenium.common.exceptions import NoAlertPresentException
+
+class SignUpTest(unittest.TestCase):
+    def setUp(self):
+        chrome_driver_path = ChromeDriverManager().install()
+        service = Service(executable_path=chrome_driver_path)
+        self.driver = webdriver.Chrome(service=service)
+        self.driver.maximize_window()
+
+    def test_a_success_signup(self):
+        #Navigate to Login Form
+        self.driver.get("https://www.demoblaze.com/")
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID,'nava')))
+        self.driver.find_element(By.ID,'signin2').click()
+        time.sleep(1)
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID,'signInModalLabel')))
+
+        #Signup Process
+        self.driver.find_element(By.ID,'sign-username').send_keys('panjibuds')
+        self.driver.find_element(By.ID,'sign-password').send_keys('test123')
+        self.driver.find_element(By.XPATH,'//*[@id="signInModal"]/div/div/div[3]/button[2]')
+
+        #Asserting the success alert
+        try :
+            alert = self.driver.switch_to.alert
+            assert "Sign up successful." in alert.text
+            alert.accept()
+        except:
+            print("No alert present")
+        
+        self.assertTrue(self.driver.find_element(By.ID,'nava').text,"PRODUCT STORE")
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
