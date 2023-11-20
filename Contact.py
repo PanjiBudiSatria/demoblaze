4cd8a11c239079ee419e63c5a10553436d47ecc4
Panji Budi Satria <panjibudi467@gmail.com>
2023-11-20 11:51:21 +0700

    New Commit

--- Contact.py
+++ Contact.py
@@ -1,9 +1,6 @@
 import unittest
 import time
 import sys
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
